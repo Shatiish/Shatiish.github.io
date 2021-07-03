9f3f0fd3d0b4940b0bf666769a5e848583df8a4e
Shatiish <86873682+Shatiish@users.noreply.github.com>
2021-07-03 19:59:10 +0800

    Version 1,2
    L and S Website. Assignment 2

---
+++ j.js
@@ -0,0 +1,24 @@
+window.onload = ()=>{
+    const loader = document.getElementById('loader');
+    loader.classList.add('loaded');
+}
+'use strict';
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+
+  open.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+}
